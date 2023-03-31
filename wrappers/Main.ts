bcbf663d3b2de85a8e8dfd32d0d391c9b323112f
1ixi1 <https://1ixi1@github.com>
2023-04-01 01:06:08 +0300

    Save 1

--- wrappers/Main.ts
+++ wrappers/Main.ts
@@ -1,9 +1,43 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-export type MainConfig = {};
+// storage#_ microusd_per_ton: uint32  // 2.3232 USD -> 2323200
+//     min_cr: uint16  // 1.5 -> 1500
+//     last_update: uint64
+//     vaults: (HashmapE 267 Vault)
+//     oracle_address: MsgAddressInt
+//     jetton_master_address: MsgAddressInt
+//     jetton_wallet_code: ^Cell
+//     = Storage;
+
+export type MainConfig = {
+    initTONPrice: number,
+    minCollateralRatio: number,
+    oracle: Address,
+    jettonMaster: Address,
+    jettonWalletCode: Cell,
+    now: number
+};
+
+export function formatPrice(price: number): number {
+    // USD per TON -> microUSD per TON
+    return Math.round(price * 10**6);
+}
+
+export function formatCollateralRatio(ratio: number): number {
+    // 1.5 -> 1500
+    return Math.round(ratio * 10**3);
+}
 
 export function mainConfigToCell(config: MainConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+            .storeUint(formatPrice(config.initTONPrice), 32)
+            .storeUint(formatCollateralRatio(config.minCollateralRatio), 16)
+            .storeUint(config.now, 64)
+            .storeDict()
+            .storeAddress(config.oracle)
+            .storeAddress(config.jettonMaster)
+            .storeRef(config.jettonWalletCode)
+           .endCell();
 }
 
 export class Main implements Contract {
